ecc15f54e93a97fe74fe0b12ccb003f781d990f9
Marcos <marcospernica@gmail.com>
2017-09-18 22:53:19 -0300

    Autenticacion en progreso

---
+++ lanzar.sh
@@ -0,0 +1,2 @@
+sudo ./bdock.sh
+sudo ./rdock.sh
--- src/server.py
+++ src/server.py
@@ -1,11 +1,17 @@
 import logging
+from flask import request
+
 from os import environ
 
 from flask import Flask
 from src import funcionesRdm
+from src import auth
 from src.views import index
 from logging.config import fileConfig
 from flask_pymongo import PyMongo
+
+from urllib import unquote
+from flask import json
 
 app = Flask(__name__)
 app.config['MONGO_DBNAME'] = 'fiuberappserver'
@@ -41,9 +47,49 @@ def probarDB():
           mostrar += str(documento)+"<br>"
 	return mostrar
 
-@app.route('/token')
+@app.route('/token', methods=['POST'])
 def autenticar():
-	return "{\"token\": \"jhgdfsghdsfagHGF342fdTrftyIUU786453\" }"
+	"""!@brief Autentica al usuario una unica vez."""
+
+	"""Si no se recibieron los datos todo mal."""	
+	datos = request.get_json(silent=True);
+	if(not datos):
+		return "Cagaste", 404
+
+	"""Se validan que esten todos los datos necesarios."""
+	nombreUsuario =  datos['nombreUsuario']
+	contrasena =  datos['contrasena']
+
+	if(not nombreUsuario or not contrasena):
+		return "Cagaste", 404
+
+	token = auth.recuperarToken(nombreUsuario);
+
+	"""Si no esta el token puede ser que no este autenticado todavia."""
+	if(not token):
+		"""
+			Pegarle al server para ver si existe el usuario y si existe crearle un token y agregarlo
+		"""
+		existe = True;
+
+		if(not existe):
+			return "Cagaste", 404
+	
+		token = auth.generarToken(nombreUsuario, contrasena)
+		if(not token):
+			return "Cagaste", 404
+
+		"""Si no se puede almacenar no tiene sentido seguir aunque el token se haya generado"""
+		if(not almacenarToken(nombreUsuario, token)):
+			return "Cagaste", 404
+		return token
+	
+	"""Si es valido se le envia y sino error."""
+	autentico = auth.validarToken(nombreUsuario, contrasena, token)
+	if(not autentico):
+		return "Cagaste", 404
+
+	return token
 
 if __name__ == '__main__':
 	port = os.environ.get('PORT', 5000)
