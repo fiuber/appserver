e142e358f093929ff49070aef9d392e58696cc21
Marcos <marcospernica@gmail.com>
2017-10-11 13:49:27 -0300

    Sale Bearer entra api-key

--- src/models/conectividad.py
+++ src/models/conectividad.py
@@ -29,7 +29,7 @@ class Conectividad(Resource):
 		@param diccionarioCuerpo Los pares clave-valor (en forma de diccionario) a enviar en el cuerpo de la peticion.
 		@param diccionarioParametros Los pares clave-valor (en forma de diccionario) a enviar como parametros de la peticion."""
 
-		headers = {'content-type': 'application/json', 'Authorization': 'Bearer '+self.appServerToken}
+		headers = {'content-type': 'application/json', 'Authorization': 'api-key '+self.appServerToken}
 		r = requests.post(self.URL+'/'+endpoint,data = json.dumps(diccionarioCuerpo), headers=headers, params = diccionarioParametros)
 		if(r.status_code != 200):
 			return False
@@ -45,7 +45,7 @@ class Conectividad(Resource):
 		@param endpoint El nombre del endpoint especifico sin la URL base ni el caracter '/'. Ej: 'user'.
 		@param diccionarioParametros Los pares clave-valor (en forma de diccionario) a enviar como parametros de la peticion."""
 
-		headers = {'content-type': 'application/json', 'Authorization': 'Bearer '+self.appServerToken}
+		headers = {'content-type': 'application/json', 'Authorization': 'api-key '+self.appServerToken}
 		r = requests.get(self.URL+'/'+endpoint, headers=headers, params = diccionarioParametros)
 		if(r.status_code != 200):
 			return False
@@ -62,7 +62,7 @@ class Conectividad(Resource):
 		@param diccionarioCuerpo Los pares clave-valor (en forma de diccionario) a enviar en el cuerpo de la peticion.
 		@param diccionarioParametros Los pares clave-valor (en forma de diccionario) a enviar como parametros de la peticion."""
 
-		headers = {'content-type': 'application/json', 'Authorization': 'Bearer '+self.appServerToken}
+		headers = {'content-type': 'application/json', 'Authorization': 'api-key '+self.appServerToken}
 		r = requests.put(self.URL+'/'+endpoint,data = json.dumps(diccionarioCuerpo), headers=headers, params = diccionarioParametros)
 		if(r.status_code != 200):
 			return False
@@ -79,7 +79,7 @@ class Conectividad(Resource):
 		@param diccionarioCuerpo Los pares clave-valor (en forma de diccionario) a enviar en el cuerpo de la peticion.
 		@param diccionarioParametros Los pares clave-valor (en forma de diccionario) a enviar como parametros de la peticion."""
 
-		headers = {'content-type': 'application/json', 'Authorization': 'Bearer '+self.appServerToken}
+		headers = {'content-type': 'application/json', 'Authorization': 'api-key '+self.appServerToken}
 		r = requests.delete(self.URL+'/'+endpoint,data = json.dumps(diccionarioCuerpo), headers=headers, params = diccionarioParametros)
 		if(r.status_code != 200):
 			return False
